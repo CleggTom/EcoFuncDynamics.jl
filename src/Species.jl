724c228a914e1421e9f090e445e0d4621e66149a
Tom <t.clegg17@imperial.ac.uk>
2018-01-17 14:02:08 +0000

    :sparkles: Added ϵ (Carbon use efficiency)

--- src/Species.jl
+++ src/Species.jl
@@ -12,7 +12,8 @@ Custom type that contains the parameter values for each species.
 * `E_p::Float64` : The temperature sensitivity of  photosynthetic rate.
 * `R0_r::Float64` : The normalisation constant of respiration rate.
 * `E_r::Float64` : The temperature sensitivity of  respiration rate.
-* `K_s::Float64` : The half saturation densitiy of nutrient uptake
+* `K_s::Float64` : The half saturation densitiy of nutrient uptake.
+* 'ϵ::Float64' : The uptake efficency
 """
 struct Species
     R0_p::Float64
@@ -20,21 +21,25 @@ struct Species
     R0_r::Float64
     E_r::Float64
     K_s::Float64
+    ϵ::Float64
 end
 
 
 """
 # Generate Community
 
-    community(S::Int64,R0_p::Float64,E_p::Float64,R0_r::Float64,E_r::Float64,K_s::Float64)
+    community(S::Int64,R0_p::Float64,E_p::Float64,
+                       R0_r::Float64,E_r::Float64,
+                       K_s::Float64 ,ϵ::Float64)
 
-Generates a community of identical species given `R0, E and K_s` values and the
-number of species.
+Generates a community of identical species given `R0, E,  K_s and ϵ` values and
+the number of species.
 """
 function community(S::Int64,R0_p::Float64,E_p::Float64,
-                            R0_r::Float64,E_r::Float64,K_s::Float64)
+                            R0_r::Float64,E_r::Float64,
+                            K_s::Float64 ,ϵ::Float64)
 
-    return(fill(Species(R0_p,E_p,R0_r,E_r,K_s),S))
+    return(fill(Species(R0_p,E_p,R0_r,E_r,K_s,ϵ),S))
 end
 
 """
@@ -45,15 +50,16 @@ Can also take arrays giving specific values
 """
 function community(R0_p::Array{Float64},E_p::Array{Float64},
                    R0_r::Array{Float64},E_r::Array{Float64},
-                   K_s::Array{Float64})
+                   K_s::Array{Float64} ,ϵ::Array{Float64})
 
     @assert length(R0_p) == length(E_p) ==
             length(R0_r) == length(E_r) ==
-            length(K_s)
+            length(K_s)  == length(ϵ)
 
     sp = Array{Species}(size(R0,1))
+
     for i = 1:size(R0,1)
-        sp[i] = Species(R0_p[i],E_p[i],R0_r[i],E_r[i],K_s[i])
+        sp[i] = Species(R0_p[i],E_p[i],R0_r[i],E_r[i],K_s[i],ϵ[i])
     end
     return(sp)
 end
