2684f83d61e65c154e0a2786260f5dadee731f5e
Tom <t.clegg17@imperial.ac.uk>
2018-04-09 10:52:13 +0100

    :memo: adds  docs

--- src/Simulate.jl
+++ src/Simulate.jl
@@ -3,15 +3,23 @@
 """
 # Simulate
 
+    simulate(p::Dict{Symbol,Any}, u0::Array{Float64},start::Int64=0, stop::Int64=500)
+
+Preforms the simulation over a given time period.
+
+## Arguments
+- 'p::Dict{Symbol,Any}' : Dictionary containing parameters (see `make_parameters`).
+- `u0::Array{Float64}` : Initial biomass period.
+- 'start::Int64=0' : Starting timepoint.
+- 'stop::Int64=500' : End timepoint.
 
 """
 
 function simulate(p::Dict{Symbol,Any}, u0::Array{Float64},
-    start::Int64=0, stop::Int64=500, use::Symbol=:nonstiff)
+    start::Int64=0, stop::Int64=500)
 
     @assert stop > start
     @assert length(u0) == p[:Eco].N_sp
-    @assert use ∈ vec([:stiff :nonstiff])
     @assert length(find(x -> isa(x,Cpool) , p[:Eco].sp)) == 1
     @assert length(find(x -> isa(x,Spool) , p[:Eco].sp)) == 1
 
@@ -21,14 +29,7 @@ function simulate(p::Dict{Symbol,Any}, u0::Array{Float64},
 
     # Perform the actual integration
     prob = ODEProblem(dcdt, u0, t, p)
-
-    if use == :stiff
-    alg = Rodas4()
-    else
-    alg = Tsit5()
-    end
-
-    sol = solve(prob, alg,  dtmax = 1, saveat=t_keep, dense=false,
+    sol = solve(prob ,  dtmax = 1, saveat=t_keep, dense=false,
     save_timeseries=false, maxiters = 1e10)
 
     return(sol)
